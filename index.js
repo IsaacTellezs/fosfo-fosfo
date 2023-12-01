0fd07a7520c3f7c5c6e81e6547e4cc07e10b15ce
Isaac Tellez <andreescalante34@gmail.com>
2023-12-01 00:43:17 -0600

    mas info

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
         function obtenerPronostico(ciudad) {
             
             const apiKey = "0afb351050fafd0b9d66a72a606dede3";
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&lang=es&appid=${apiKey}`;
 
             fetch(url)
                 .then(response => response.json())
@@ -21,11 +21,22 @@
             const temperaturaCelsius = (data.main.temp - 273.15).toFixed(2);
             const velocidadViento = data.wind.speed;
             const direccionViento = data.wind.deg;
+            const presionAtmosferica = data.main.pressure;
             let mensajeViento = '';
             const umbralVientoAlto = 3;
 
-            if (velocidadViento > umbralVientoAlto) {
-                mensajeViento = '¡Viento fuerte! Precaución.';
+            if (velocidadViento >= 33 && velocidadViento <= 42) {
+                mensajeViento ="Categoría 1: Velocidad del viento entre 119-153 km/h (33-42 m/s)";
+            } else if (velocidadViento >= 43 && velocidadViento <= 49) {
+                mensajeViento ="Categoría 2: Velocidad del viento entre 154-177 km/h (43-49 m/s)";
+            } else if (velocidadViento >= 50 && velocidadViento <= 58) {
+                mensajeViento ="Categoría 3: Velocidad del viento entre 178-208 km/h (50-58 m/s)";
+            } else if (velocidadViento >= 59 && velocidadViento <= 70) {
+                mensajeViento ="Categoría 4: Velocidad del viento entre 209-251 km/h (59-70 m/s)";
+            } else if (velocidadViento > 70) {
+                mensajeViento ="Categoría 5: Velocidad del viento superior a 251 km/h (>70 m/s)";
+            } else {
+                mensajeViento ="";
             }
 
     resultadoContainer.innerHTML = `
@@ -34,6 +45,7 @@
         <p>Condiciones: ${data.weather[0].description}</p>
         <p>Velocidad del Viento: ${velocidadViento} m/s</p>
         <p>Dirección del Viento: ${direccionViento}°</p>
+        <p> Presión atmosferica: ${presionAtmosferica} hPa</p>
         <p>${mensajeViento}</p>
         
     `;
